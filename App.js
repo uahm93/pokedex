b78a0eaf45332254e37864867a8b35ea6aed1357
Ulises Hernandez <desarrollo15@xpd.mx>
2022-01-13 09:56:33 -0600

    Se Agregaron librerias para navegacion

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+/* Screens */
+import DetailScreen from './source/components/Details';
+
+const HomeScreen = () => {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+
 }
+
+const App = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Details: {
+    screen: DetailScreen
+  }
+}, { initialRouteName: 'Details' });
+
+export default createAppContainer(App);
 
 const styles = StyleSheet.create({
   container: {
